6acaf4ced986bebf75c6c3c573f516251a31afa3
delphijedi <luke.blyth@gmail.com>
2018-04-28 21:13:36 +0100

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,12 @@ import { AlertifyService } from './_services/alertify.service';
 import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
 import { JwtModuleOptions} from '@auth0/angular-jwt';
 import { BsDropdownModule} from 'ngx-bootstrap';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 @NgModule({
@@ -21,16 +27,21 @@ import { BsDropdownModule} from 'ngx-bootstrap';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
 ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService,
-  AlertifyService],
+  AlertifyService,
+  AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import {AlertifyService} from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,7 @@ import {AlertifyService} from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private altertifyService: AlertifyService) { }
+  constructor(public authService: AuthService, private altertifyService: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -20,7 +21,9 @@ export class NavComponent implements OnInit {
        this.altertifyService.success('logged in successfully');
 
     }, error => {
-      this.altertifyService.error(error);
+      this.altertifyService.error('Failed to login');
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -28,6 +31,7 @@ export class NavComponent implements OnInit {
     this.authService.userToken = null;
     localStorage.removeItem('token');
     this.altertifyService.message('Logged out');
+    this.router.navigate(['/home']);
   }
 
   loggedIn() {
